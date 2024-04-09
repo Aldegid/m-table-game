b05ffb12d079d76af5df1ca8b063972b706657d7
Andrey394 <andrey394@hitech18.online>
2024-04-09 15:00:05 +0300

    add lessons data and generate lessons logic

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -4,13 +4,22 @@ import classes from './card.module.scss';
 import error from '../../assets/error.svg';
 import success from '../../assets/success.svg';
 
-const DATA = { task: '7 x 8', answers: [56, 65, 48], answer: 56 };
+import { getRandomElement, shuffleArray } from '../../helpers/helpers';
 
-export const Card = () => {
+import { LESSONS } from '../../data/lessons';
+
+type Answer = null | number;
+
+export const Card: React.FC = () => {
   const [isFlipped, setIsFlipped] = useState(false);
-  const [answer, setAnswer] = useState(null);
+  const [answer, setAnswer] = useState<Answer>(null);
+  const [currentLesson, setCurrentLesson] = useState(LESSONS[0]);
 
-  const handleCardClick = (answer: any) => {
+  const rightAnswer = answer === currentLesson.answer;
+
+  console.log(answer, currentLesson.answer);
+
+  const handleCardClick = (answer: number) => {
     setAnswer(answer);
   };
 
@@ -19,22 +28,32 @@ export const Card = () => {
     setIsFlipped(false);
   };
 
+  const getNextLesson = () => {
+    setCurrentLesson(getRandomElement(LESSONS));
+  };
+
+  const successAction = () => {
+    getNextLesson();
+    resetBoard();
+  };
+
   useEffect(() => {
     if (answer) {
       setIsFlipped(true);
     }
   }, [answer]);
 
-  const rightAnswer = answer === DATA.answer;
+  const suffleAnswers = shuffleArray(currentLesson.answers);
 
   return (
     <div className={`${classes.card} ${isFlipped ? classes.flipped : ''}`}>
+      <pre>{JSON.stringify(currentLesson, null, 2)}</pre>
       <div className={classes.card_inner}>
         <div className={classes.card_front}>
-          <p className={classes.task}>7 x 8</p>
+          <p className={classes.task}>{currentLesson.task}</p>
           <br />
           <div className={classes.buttons}>
-            {DATA.answers.map((answer) => {
+            {suffleAnswers.map((answer) => {
               return (
                 <button
                   className={classes.buttons_btn}
@@ -63,13 +82,16 @@ export const Card = () => {
               )}
             </div>
             <div className={classes.buttons}>
-              <button className={classes.buttons_btn} onClick={resetBoard}>
+              <button
+                className={classes.buttons_btn}
+                onClick={rightAnswer ? successAction : resetBoard}
+              >
                 {rightAnswer ? 'Next exercise' : 'Try again'}
               </button>
             </div>
           </div>
         ) : (
-          <div className={classes.card_back}></div>
+          <div className={`${classes.card_back} ${classes.card_blank}`}></div>
         )}
       </div>
     </div>
